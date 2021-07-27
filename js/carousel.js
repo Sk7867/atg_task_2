448e794e39b679d52dff35487c23f591baa7495a
Siddhesh Kamble <siddhesh.kamble7867@gmail.com>
2021-07-27 20:16:40 +0530

    initial commit

---
+++ js/carousel.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+  loop: false,
+  margin: 0,
+  // autoplay: true,
+  // autoplayTimeout: 3500,
+  nav: false,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
---
+++ js/imgAnimate.js
@@ -0,0 +1,250 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.sec1_img1, .sec1_img2, .heading, .subheading', {
+  scrollTrigger: {
+    trigger: '#section_1',
+    start: 'center bottom',
+    end: 'top bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  y: '100%',
+  duration: 1,
+  stagger: 0.3,
+  opacity: 0,
+  ease: 'power1.in',
+});
+
+gsap.from('.case_study', {
+  scrollTrigger: {
+    trigger: '#section_1',
+    start: 'center bottom',
+    end: 'top bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  duration: 1,
+  stagger: 0.3,
+  opacity: 0,
+  delay: 0.7,
+  transfromOrigin: 'center',
+  ease: 'power1.in',
+});
+
+gsap.from('.sec2_img1', {
+  scrollTrigger: {
+    trigger: '#section_2',
+    start: 'center bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  y: '100%',
+  duration: 1,
+  ease: 'power1.in',
+});
+gsap.from('.sec2_img2', {
+  scrollTrigger: {
+    trigger: '#section_2',
+    start: 'center bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  y: '-100%',
+  duration: 0.7,
+  ease: 'power1.in',
+});
+
+gsap.from('.sec3_img1', {
+  scrollTrigger: {
+    trigger: '#section_3',
+    start: 'center bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  y: '100%',
+  opacity: 0,
+  duration: 0.7,
+  ease: 'power1.in',
+});
+gsap.from('.sec3_img2', {
+  scrollTrigger: {
+    trigger: '#section_3',
+    start: 'center bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  y: '-100%',
+  opacity: 0,
+  duration: 0.7,
+  delay: 0.2,
+  ease: 'power1.in',
+});
+
+gsap.from('.sec4_img1', {
+  scrollTrigger: {
+    trigger: '#section_4',
+    start: 'center bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  y: '100%',
+  opacity: 0,
+  duration: 0.5,
+  ease: 'power1.in',
+});
+gsap.from('.sec4_img2', {
+  scrollTrigger: {
+    trigger: '#section_4',
+    start: 'center bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  y: '-100%',
+  opacity: 0,
+  duration: 0.7,
+  delay: 0.2,
+  ease: 'power1.in',
+});
+gsap.from('.sec4_img4', {
+  scrollTrigger: {
+    trigger: '#section_4',
+    start: 'center bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  y: '-100%',
+  x: '-50%',
+  opacity: 0,
+  duration: 0.7,
+  delay: 0.2,
+  ease: 'power1.in',
+});
+gsap.from('.sec4_img5', {
+  scrollTrigger: {
+    trigger: '#section_4',
+    start: 'center bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  y: '-100%',
+  opacity: 0,
+  duration: 0.7,
+  delay: 0.2,
+  ease: 'power1.in',
+});
+gsap.from('.sec4_img7', {
+  scrollTrigger: {
+    trigger: '#section_4',
+    start: 'center bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  y: '100%',
+  x: '100%',
+  opacity: 0,
+  duration: 0.7,
+  delay: 0.2,
+  ease: 'power1.in',
+});
+gsap.from('.sec5_img1', {
+  scrollTrigger: {
+    trigger: '#section_5',
+    start: 'center bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  x: '100%',
+  opacity: 0,
+  duration: 0.7,
+  ease: 'power1.in',
+});
+gsap.from('.sec5_img2', {
+  scrollTrigger: {
+    trigger: '#section_5',
+    start: 'center bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  y: '-100%',
+  opacity: 0,
+  duration: 0.7,
+  delay: 0.2,
+  ease: 'power1.in',
+});
+gsap.from('.sec5_img3', {
+  scrollTrigger: {
+    trigger: '#section_5',
+    start: 'center bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  y: '100%',
+  opacity: 0,
+  duration: 0.7,
+  delay: 0.2,
+  ease: 'power1.in',
+});
+gsap.from('.sec5_img4', {
+  scrollTrigger: {
+    trigger: '#section_5',
+    start: 'center bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  y: '-100%',
+  x: '-50%',
+  opacity: 0,
+  duration: 0.7,
+  delay: 0.2,
+  ease: 'power1.in',
+});
+gsap.from('.sec5_img5', {
+  scrollTrigger: {
+    trigger: '#section_5',
+    start: 'center bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  y: '100%',
+  opacity: 0,
+  duration: 0.7,
+  delay: 0.2,
+  ease: 'power1.in',
+});
+gsap.from('.sec6_img1', {
+  scrollTrigger: {
+    trigger: '#section_6',
+    start: 'center bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  y: '100%',
+  opacity: 0,
+  duration: 0.7,
+  ease: 'power1.in',
+});
+gsap.from('.sec7_img1', {
+  scrollTrigger: {
+    trigger: '#section_7',
+    start: 'center bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  y: '100%',
+  opacity: 0,
+  duration: 0.7,
+  ease: 'power1.in',
+});
+gsap.from('.sec7_img2', {
+  scrollTrigger: {
+    trigger: '#section_7',
+    start: 'center bottom',
+    markers: true,
+    toggleActions: 'restart none none reverse',
+  },
+  y: '100%',
+  opacity: 0,
+  duration: 0.7,
+  delay: 0.2,
+  ease: 'power1.in',
+});
